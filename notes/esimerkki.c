a7fc0be431b4452cdd8366532fdd8922a527401c
matiasSuksi <suksimatias@gmail.com>
2023-10-24 00:06:25 +0300

    Working on 2nd chapter

--- notes/esimerkki.c
+++ notes/esimerkki.c
@@ -1,13 +1,15 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int i;                                                  //Alustamaton muuttuja --> alustamaton data
-int n = 1;                                              //Alustettu muuttuja --> alustettu data
+int i;                                             //Alustamaton muuttuja --> alustamaton data
+int n = 1;                                         //Alustettu muuttuja --> alustettu data
 
-int main(void)                                          //Funktiokutsu --> Pino
+int main(void)                                     //Funktiokutsu --> Pino
 {  
-    int numero = 10;                                    //Paikallinen muuttuja --> Pino
-    int* pointteri = (int*)malloc(n * sizeof(int));     //Dynaamisesti allokoitu muistilohko --> Keko
-    free(pointteri);
+    int numero = 10;                               //Paikallinen muuttuja --> Pino
+    int* osoitin = (int*)malloc(n * sizeof(int));  //Dynaamisesti allokoitu muistilohko --> Keko
+    free(osoitin);                                 //Dynaamisesti vapautettu muistilohko --> Vapautettu keosta
     return 0;
 }
